25f4658124e45ff1d3b667de663483af4b5d966d
Paul Närhi <paulnarhi@gmail.com>
2024-12-04 11:56:38 +0200

    Day 1

---
+++ day1.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+
+# a)
+file = open('input_day1.txt', 'r')
+content = file.read()
+rows = content.split("\n")
+first_col = [int(r.split("   ")[0]) for r in rows]
+second_col = [int(r.split("   ")[1]) for r in rows]
+print(np.sum(np.abs(np.subtract(np.sort(first_col), np.sort(second_col)))))
+
+# b)
+file = open('input_day1.txt', 'r')
+content = file.read()
+rows = content.split("\n")
+first_col = [int(r.split("   ")[0]) for r in rows]
+second_col = np.array([int(r.split("   ")[1]) for r in rows])
+
+sim_score = 0
+for n in first_col:
+    count = sum(second_col == n)
+    sim_score += count * n
+print(sim_score)

e033fe482fc3e4a0171326321fd02216465fc0bb
Paul Närhi <paulnarhi@gmail.com>
2024-12-04 12:26:54 +0200

    Add encrypted inputs to allow multi computer use

---
+++ encryptor.py
@@ -0,0 +1,28 @@
+import os
+from dotenv import load_dotenv
+from cryptography.fernet import Fernet
+import sys
+
+load_dotenv()
+fernet = Fernet(os.getenv('FERNET_KEY'))
+
+day = sys.argv[1]
+operation = sys.argv[2]
+
+if operation == 'e':
+    with open(f'input_day{day}.txt', 'r') as file:
+        content = file.read()
+    encMessage = fernet.encrypt(content.encode())
+    # Write the encrypted content as raw bytes
+    with open(f'public_input_day{day}.txt', 'wb') as file:
+        file.write(encMessage)
+
+elif operation == 'd':
+    with open(f'public_input_day{day}.txt', 'rb') as file:
+        encMessage = file.read()
+    decMessage = fernet.decrypt(encMessage).decode()
+    with open(f'input_day{day}.txt', 'w') as file:
+        file.write(decMessage)
+
+else:
+    print("Invalid operation. Use 'e' for encrypt or 'd' for decrypt.")
